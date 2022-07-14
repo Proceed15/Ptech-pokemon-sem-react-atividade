58f56928fe5614d9433cf514c4187491168732b9
Jose <jlisboa600@gmail.com>
2022-07-14 00:11:47 -0300

    commit inicial

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+listPokemons();
+
+function listPokemons() {
+    //https://bit.ly/3MJecd5
+    //Usando o link acima tem que adicionar um por um.
+    fetch('https://6283929f92a6a5e462260498.mockapi.io/pokemons')
+        .then(response => response.json())
+        .then(data => {
+            const pokemons = data.map((item) => getContentCard(item));
+
+            document.querySelector(".card__container").innerHTML = pokemons.join('');
+        });
+}
+
+function getContentCard(data) {
+    return `
+    <a id="${data.id}" class="card" href="/${data.id}.html" style="background-color:${data.color || 'red'}">
+      <img class="card__image" src="/assets/images/stat/stat-${data.avatar}.png" alt="${data.name}">
+      <div class="card__title"> ${data.name}</div>
+    </a>
+    `;
+}
